3756e1de2c20dd0335040fdd970421df95601b43
jmpfbmx <jmpf.bmx@gmail.com>
2019-12-28 22:45:45 +0100

    msm: vidc: Handling of QBUF wihtout register
    Clean and modified code to support WFD use case, where WFD queues
    input buffer without prepare call.
    
    Change-Id: I46d1a507f529f46d49116b948da6304c5aeb1ea1
    Signed-off-by: Sanjay Singh <sisanj@codeaurora.org>

--- make_defconfig.sh
+++ make_defconfig.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+<<<<<<< 494c8952732045edeea5b70d4677aac289328453
 BUILD_SUBARCH=$1
 if [ "$BUILD_SUBARCH" = "arm" ]; then
 	GCC_SUBDIR=arm-eabi
@@ -8,18 +9,27 @@ elif [ "$BUILD_SUBARCH" = "arm64" ]; then
 fi
 
 DEFCONFIG_FILE=$2
+=======
+DEFCONFIG_FILE=$1
+>>>>>>> Add make_defconfig for easier update of kernel defconfigs
 if [ -z "$DEFCONFIG_FILE" ]; then
 	echo "Need defconfig file(msm_defconfig)!"
 	exit -1
 fi
 
+<<<<<<< 494c8952732045edeea5b70d4677aac289328453
 if [ ! -e arch/$BUILD_SUBARCH/configs/$DEFCONFIG_FILE ]; then
 	echo "No such file : arch/$BUILD_SUBARCH/configs/$DEFCONFIG_FILE"
+=======
+if [ ! -e arch/arm/configs/$DEFCONFIG_FILE ]; then
+	echo "No such file : arch/arm/configs/$DEFCONFIG_FILE"
+>>>>>>> Add make_defconfig for easier update of kernel defconfigs
 	exit -1
 fi
 
 # make .config
 env KCONFIG_NOTIMESTAMP=true \
+<<<<<<< 494c8952732045edeea5b70d4677aac289328453
 make ARCH=$BUILD_SUBARCH CROSS_COMPILE=$GCC_SUBDIR- ${DEFCONFIG_FILE}
 
 # run menuconfig
@@ -29,6 +39,17 @@ make savedefconfig ARCH=$BUILD_SUBARCH
 
 # copy .config to defconfig
 mv defconfig arch/$BUILD_SUBARCH/configs/${DEFCONFIG_FILE}
+=======
+make ARCH=arm CROSS_COMPILE=arm-eabi- ${DEFCONFIG_FILE}
+
+# run menuconfig
+env KCONFIG_NOTIMESTAMP=true \
+make menuconfig ARCH=arm
+make savedefconfig ARCH=arm
+
+# copy .config to defconfig
+mv defconfig arch/arm/configs/${DEFCONFIG_FILE}
+>>>>>>> Add make_defconfig for easier update of kernel defconfigs
 
 # clean kernel object
 make mrproper
